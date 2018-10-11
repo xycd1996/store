bd11d10c1356fe94f5b8ca1f15884dc97d63fad5
kite <515838785@qq.com>
2018-10-12 02:27:15 +0800

    商城小程序

---
+++ components/menu/menu.js
@@ -0,0 +1,74 @@
+// components/menu/menu.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    menus: [
+      {
+        title: "登录",
+        img: "./images/login.png",
+        url: ""
+      },
+      {
+        title: "全部商品",
+        img: "./images/all.png",
+        url: ""
+      },
+      {
+        title: "购物车",
+        img: "./images/cart.png",
+        url: ""
+      },
+      {
+        title: "全部订单",
+        img: "./images/order.png",
+        url: ""
+      },
+      {
+        title: "分类",
+        img: "./images/classfiy.png",
+        url: ""
+      },
+      {
+        title: "注册有礼",
+        img: "./images/gift.png",
+        url: ""
+      },
+      {
+        title: "限时抢购",
+        img: "./images/limit.png",
+        url: ""
+      },
+      {
+        title: "积分商城",
+        img: "./images/integral.png",
+        url: ""
+      },
+      {
+        title: "个人中心",
+        img: "./images/center.png",
+        url: ""
+      },
+      {
+        title: "客服",
+        img: "./images/service.png",
+        url: ""
+      }
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ components/slider/slider.js
@@ -0,0 +1,16 @@
+// components/slider/slider.js
+Component({
+  data: {
+    imgUrls: [
+      './images/banner_1.png',
+      './images/banner_2.png'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    circular: true,
+    indicatorColor: 'rgb(255, 255, 255)',
+    indicatorActiveColor: 'rgb(242, 57, 91)',
+    interval: 5000,
+    duration: 1000
+  }
+})
